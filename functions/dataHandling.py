b9f139f18f44d370b90e91c053c58d1fc61efac4
BvDod <43404889+BvDod@users.noreply.github.com>
2025-04-05 17:28:54 +0200

    added support for CRC, and dont take cancerous samples

--- functions/dataHandling.py
+++ functions/dataHandling.py
@@ -34,6 +34,23 @@ def get_dataset(dataset_name: str, print_stats = False, disable_augmentation = F
             transforms.Resize((256, 256)),
             transforms.ToTensor(),])
         
+    elif dataset_name == "CRC":
+        if not disable_augmentation:
+            img_transforms = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.2, hue=0.1),
+                transforms.RandomAffine(3, scale=(0.95, 1.05)),
+                transforms.ToTensor(),
+            ])
+            
+        elif disable_augmentation:
+            img_transforms = transforms.Compose([
+                transforms.ToTensor(),]
+            )
+
+        transforms_val = transforms.Compose([
+            transforms.ToTensor(),])
+       
     else:   
         img_transforms = transforms.Compose([                                     
             transforms.ToTensor(),]
@@ -61,12 +78,30 @@ def get_dataset(dataset_name: str, print_stats = False, disable_augmentation = F
         var = 0.1102 # Precomputed
         channels = 1
     
-    if dataset_name == "celebA":
+    elif dataset_name == "celebA":
         train = torchvision.datasets.ImageFolder("./datasets/celeba_hq/train/", transform=img_transforms)
         test = torchvision.datasets.ImageFolder("./datasets/celeba_hq/val/", transform=transforms_val)
         input_shape = (256, 256)
         channels = 3
         var = 0.0692 # Precomputed
+    
+    elif dataset_name == "CRC":
+        train = torchvision.datasets.ImageFolder("./CRC/train/", transform=img_transforms)
+        test = torchvision.datasets.ImageFolder("./CRC/val/", transform=transforms_val)
+        from torch.utils.data import Subset
+        class_to_idx = train.class_to_idx
+
+        filtered_train = [i for i, (_, label) in enumerate(train.samples) if not (label == 7 or label == 8)]
+        filtered_test = [i for i, (_, label) in enumerate(test.samples) if not (label == 7 or label == 8)]
+
+        # Create a subset
+        train = Subset(train, filtered_train)
+        test = Subset(test, filtered_test)
+
+        print(class_to_idx)
+        input_shape = (224, 224)
+        channels = 3
+        var = 0.225 # Precomputed
 
     if print_stats:
         print(f"Training dataset shape: {train[0][0].shape}, samples = {len(train)}")
